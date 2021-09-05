895dac6710a66a46c6ad90318b8593260e84e0fe
Oumaima <oumaimabk168@gmail.com>
2021-09-05 20:03:06 +0200

    Add style 'list,listItems' and add handleclick function to the sliderArrow left and right

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,18 +1,36 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons';
-import React from 'react'
+import React, { useRef } from 'react'
 import ListItems from '../ListItems/ListItems';
 import "./List.scss";
 const List = () => {
+    const listRef = useRef()
+    const handleClick=(direction)=>{
+         let distance = listRef.current.getBoundingClientRect().x - 50
+         if (direction === "left"){
+             listRef.current.style.transform = `translateX (${ 230 + distance}px)`
+         }
+         if (direction === "right"){
+            listRef.current.style.transform = `translateX (${-230 + distance}px)`
+        }
+
+    }
     return (
         <div className="list">
             <span className="listTitle">Continue to watch</span>
             <div className="wrapper">
-                <ArrowBackIosOutlined/>
-                <div className="container">
-
-                 <ListItems/>          
+                <ArrowBackIosOutlined className="sliderArrow left" onClick={()=> handleClick("left")}/>
+                <div className="container" ref={listRef}>
+                <ListItems/>  
+                <ListItems/>  
+                <ListItems/>  
+                <ListItems/>  
+                <ListItems/>  
+                <ListItems/>  
+                <ListItems/>  
+                <ListItems/>  
+                <ListItems/>          
                 </div>
-                <ArrowForwardIosOutlined/>
+                <ArrowForwardIosOutlined className="sliderArrow right" onClick={()=> handleClick("right")}/>
             </div>
         </div>
     )
